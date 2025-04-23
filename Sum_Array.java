d2f687eefe18f9c962bdfc95df244f3198094c79
Archit-dev003 <architchhirolya@gmail.com>
2025-04-24 01:02:38 +0530

    sum of two 1D array

--- Sum_Array.java
+++ Sum_Array.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class Sum_Array {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
         int[] arr1 = { 1, 2, 3, 4, 5, 6 };
         int[] arr2 = { 0, 9, 8, 7, 6, 4 };
         int[] sum = new int[arr1.length];
