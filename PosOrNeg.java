816cb958fa4ca5b34d9d5414fbb394a5c90527c1
Archit-dev003 <architchhirolya@gmail.com>
2025-04-24 00:55:40 +0530

    if num is +ve/-ve

--- PosOrNeg.java
+++ PosOrNeg.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class PosOrNeg {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+     {
         Scanner in = new Scanner(System.in);
 
         System.out.println("enter your number: ");
